301ec2b442b65d1242476ee02f173aba3f211b72
Selina <jbaek520@gmail.com>
2023-05-09 21:45:59 +0900

    project_221223 #1 로그아웃 로직

--- src/main/java/com/PersonalProject/user/UserController.java
+++ src/main/java/com/PersonalProject/user/UserController.java
@@ -1,5 +1,7 @@
 package com.PersonalProject.user;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,5 +19,16 @@ public class UserController {
 	public String signInView(Model model) {
 		model.addAttribute("view", "user/signIn");
 		return "template/layout";
+	}
+	
+	@RequestMapping("/sign_out")
+	public String signOut(HttpSession session) {
+		// 세션에 있는 모든 것을 비운다.
+		session.removeAttribute("userId");
+		session.removeAttribute("userName");
+		session.removeAttribute("userLoginId");
+		
+		// 로그인 화면 이동
+		return "redirect:/user/sign_in_view";
 	}
 }
