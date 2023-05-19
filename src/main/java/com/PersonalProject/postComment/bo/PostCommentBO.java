cd3a276b05594b743d7c564f88a4c80bfc8d96f2
박지호 <jiho2689@naver.com>
2023-05-19 21:46:17 +0900

    project_221223 #8 게시판 글 상세 페이지 댓글쓰기 기능 구현(미완)

--- src/main/java/com/PersonalProject/postComment/bo/PostCommentBO.java
+++ src/main/java/com/PersonalProject/postComment/bo/PostCommentBO.java
@@ -1,8 +1,16 @@
 package com.PersonalProject.postComment.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.PersonalProject.postComment.dao.PostCommentMapper;
 
 @Service
 public class PostCommentBO {
-
+	@Autowired
+	private PostCommentMapper postCommentMapper;
+	
+	public int addPostCommentByContentPostId(int postId, String content) {
+		return postCommentMapper;
+	}
 }
