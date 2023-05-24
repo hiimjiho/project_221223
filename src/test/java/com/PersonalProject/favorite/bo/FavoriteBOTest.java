5603e4b06d8642812156b10f3b45b3e7b9035197
DESKTOP-T9HI5KP\박지호 <jiho2689@naver.com>
2023-05-25 04:12:35 +0900

    project_221223 #13 관심상품 기능 확인

---
+++ src/test/java/com/PersonalProject/favorite/bo/FavoriteBOTest.java
@@ -0,0 +1,42 @@
+package com.PersonalProject.favorite.bo;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+@SpringBootTest
+class FavoriteBOTest {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	FavoriteBO favoriteBO;
+	
+	@Transactional
+	//@Test
+	void 관심상품추가() {
+		logger.info("######관싱삼품 추가#######");
+		favoriteBO.addFavoriteByUserIdProductId(1, 2);
+	}
+	
+	//@Test
+	void 관심상품삭제() {
+		logger.info("######관심상품 삭제#########");
+		favoriteBO.deleteFavoriteByProductId(1, 1);
+	}
+	
+	@Test
+	void 관심상품뿌리기() {
+		logger.info("########관심상품 정보 뿌리기#########");
+		favoriteBO.generateFavoriteByUserId(1);
+	}
+	
+	//@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
