8b4fb6e220294ca0888eff7b90bd52282b49c9ad
DESKTOP-T9HI5KP\박지호 <jiho2689@naver.com>
2023-06-01 17:49:22 +0900

    project_221223 #24

--- src/main/java/com/PersonalProject/config/WebMvcConfig.java
+++ src/main/java/com/PersonalProject/config/WebMvcConfig.java
@@ -1,14 +1,19 @@
 package com.PersonalProject.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.PersonalProject.common.FileManagerService;
+import com.PersonalProject.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 	
+	@Autowired
+	private PermissionInterceptor interceptor;
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -16,4 +21,12 @@ public class WebMvcConfig implements WebMvcConfigurer{
 		.addResourceHandler("/images/**") // 웹 이미지 주소 cf) http://localhost/images/aaaa_2023050601/good.png
 		.addResourceLocations("file:///" + FileManagerService.FILE_UPLOAD_PATH); // window / 3개 mac / 2개
 	}
+	
+	// intercepter 설정 추가
+		public void addInterceptors(InterceptorRegistry registry) {
+			registry
+			.addInterceptor(interceptor) // 등록할 인터셉터 설정
+			.addPathPatterns("/**")	// 적용할 URL 패턴 설정
+			.excludePathPatterns("/favicon.ico", "/error", "/static/**", "/user/sign_out");	// 인터셉터를 제외할 url 패턴 등록
+		}
 }
