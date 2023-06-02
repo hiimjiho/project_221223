13926c2e619b7dbf06122404321e3f44aec0e63a
박지호 <jiho2689@naver.com>
2023-06-02 21:41:39 +0900

    project_221223 #23 관리자 페이지 마크업

--- src/main/java/com/PersonalProject/admin/AdminController.java
+++ src/main/java/com/PersonalProject/admin/AdminController.java
@@ -1,8 +1,21 @@
 package com.PersonalProject.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/admin")
+@Controller
 public class AdminController {
+	@GetMapping("/login_page_view")
+	public String pageView() {
+		
+		return "admin/adminSignInView";
+	}
 	
+	@GetMapping("/admin_main_view")
+	public String adminMainView() {
+		
+		return "admin/adminMainView";
+	}
 }
