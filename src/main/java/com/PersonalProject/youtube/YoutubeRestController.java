6880a69f91b7c6269965428c1feb65d8257e9919
박지호 <jiho2689@naver.com>
2023-06-02 21:44:49 +0900

    project_221223 #25 유튜브 검색 API

--- src/main/java/com/PersonalProject/youtube/YoutubeRestController.java
+++ src/main/java/com/PersonalProject/youtube/YoutubeRestController.java
@@ -1,7 +1,8 @@
 package com.PersonalProject.youtube;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RequestMapping("/youtube")
 @RestController
 public class YoutubeRestController {
 
